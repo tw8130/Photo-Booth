80044b5626732ec57f858616bd1ccc113a7a073b
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-22 17:36:24 +0300

    create a view  function pics-of-day then we call the render function and pass in 3 arguments. The request, the template file, and a dictionary of values that we pass into the template

--- pics/views.py
+++ pics/views.py
@@ -4,15 +4,19 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse(request, 'welcome.html')
+    return render(request, 'welcome.html')
 
 def pics_of_day(request):
     date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-pics/today-pics.html', {"date": date,})
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
