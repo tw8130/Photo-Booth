f979a0cf646f7d9acd8c0be9223e36a8cbf0791b
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-22 22:35:17 +0300

    we change our project databse settings use  postgres then change time_zone setting to local timezone

--- booth/settings.py
+++ booth/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'booth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'booth',
+        'USER': 'user',
+    'PASSWORD':'8130254#',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
