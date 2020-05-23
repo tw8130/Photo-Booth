1aa00f2e239e78efe9f972fc64ea64346dd72db8
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-23 22:10:25 +0300

    create save ,delete,update and get location by id functions for the location model

--- pics/models.py
+++ pics/models.py
@@ -5,6 +5,21 @@ import datetime as dt
 class Location(models.Model):
     loc_name = models.CharField(max_length =30)
 
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self):
+        self.loc_name = update
+        self.save()
+    
+    @classmethod
+    def get_location_id(cls ,id):
+        location = Location.objects.get(pk =id)
+        return location
+        
     def __str__(self):
         return self.loc_name
 
@@ -17,8 +32,8 @@ class Category(models.Model):
 class Image(models.Model):
     title = models.CharField(max_length =30)
     description = models.TextField(max_length =30)
-    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING,)
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, )
+    location = models.ForeignKey(Location, on_delete=models.CASDCADE,)
+    category = models.ForeignKey(Category, on_delete=models.CASDCADE, )
     pub_date = models.DateTimeField(auto_now_add=True) 
 
     class Meta:
