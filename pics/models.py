920c51f83b384fe292533b33327cb73bae0e306b
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-23 20:06:58 +0300

    create the location,category and image model with its attributes also add meta class to allow ordering by id in image model

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    loc_name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.loc_name
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.cat_name
+
+class Image(models.Model):
+    title = models.CharField(max_length =30)
+    description = models.TextField(max_length =30)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING,)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, )
+    pub_date = models.DateTimeField(auto_now_add=True) 
+
+    class Meta:
+        ordering = ('-id',)
+    
+    def __str__(self):
+        return self.title
+
