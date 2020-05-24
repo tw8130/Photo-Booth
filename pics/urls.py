d078d19cdd89809d2e5f3ecad8dc2d095c214f09
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-24 22:47:56 +0300

    create a URL pattern with name search results

--- pics/urls.py
+++ pics/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='index'),
-    path('today/',views.pics_of_day,name='picsToday')
+    path('today/',views.pics_of_day,name='picsToday'),
+    path('search/', views.search_results, name='search_results')
 ]
