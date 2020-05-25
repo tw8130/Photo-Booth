feb5040a91f26f7b5a5bdd223f2fc755b0512e86
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-25 12:10:45 +0300

    We create an image route and we capture an integer which will be the id of the image

--- pics/urls.py
+++ pics/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,6 +8,7 @@ urlpatterns = [
     path('', views.welcome, name='index'),
     path('today/',views.pics_of_day,name='picsToday'),
     path('search/', views.search_results, name='search_results')
+    path('image/(\d+)',views.display_image, name='image')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
